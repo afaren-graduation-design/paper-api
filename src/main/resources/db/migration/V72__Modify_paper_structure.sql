9d024a5e5844e5b4e9130cf1523304dae3cc5b27
Yvette14 <libratyb1014@163.com>
2016-11-09 16:07:24 +0800

    delete one same line

--- src/main/resources/db/migration/V72__Modify_paper_structure.sql
+++ src/main/resources/db/migration/V72__Modify_paper_structure.sql
@@ -1,4 +1,3 @@
 ALTER TABLE `paper` ADD COLUMN description VARCHAR(128);
 ALTER TABLE `paper` ADD COLUMN createTime VARCHAR(128);
-ALTER TABLE `paper` ADD COLUMN isDistribution BOOLEAN ;
 ALTER TABLE `paper` ADD COLUMN isDistribution BOOLEAN DEFAULT NULL;
