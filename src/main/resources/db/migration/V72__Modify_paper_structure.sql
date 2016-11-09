80a3e86956223ed86b1f617a5bf3237c339a2056
Yvette14 <libratyb1014@163.com>
2016-11-09 16:04:03 +0800

    modify paper table structure

---
+++ src/main/resources/db/migration/V72__Modify_paper_structure.sql
@@ -0,0 +1,4 @@
+ALTER TABLE `paper` ADD COLUMN description VARCHAR(128);
+ALTER TABLE `paper` ADD COLUMN createTime VARCHAR(128);
+ALTER TABLE `paper` ADD COLUMN isDistribution BOOLEAN ;
+ALTER TABLE `paper` ADD COLUMN isDistribution BOOLEAN DEFAULT NULL;
