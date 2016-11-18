dca1687cccc6ae1bb9df09175ad78e3cb225dea9
Miss-qi <1370322806@qq.com>
2016-11-18 16:22:42 +0800

    qyz & zyj - add test for /homeworkQuizzes/ids/id1,id2,...

--- src/test/java/com/thoughtworks/twars/mapper/HomeworkSubmitMapperTest.java
+++ src/test/java/com/thoughtworks/twars/mapper/HomeworkSubmitMapperTest.java
@@ -6,7 +6,6 @@ import org.junit.Test;
 
 import java.util.List;
 
-import static com.sun.xml.internal.ws.dump.LoggingDumpTube.Position.Before;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
 
--- src/test/java/com/thoughtworks/twars/resource/HomeworkQuizResourceTest.java
+++ src/test/java/com/thoughtworks/twars/resource/HomeworkQuizResourceTest.java
@@ -29,4 +29,20 @@ public class HomeworkQuizResourceTest extends TestBase {
         Response response = target(basePath + "/1").request().get();
         assertThat(response.getStatus(), is(200));
     }
+
+    @Test
+    public void should_return_homework_quiz_List_by_ids() {
+        when(homeworkQuizMapper.findById(1)).thenReturn(homeworkQuiz);
+        when(homeworkQuiz.getDescription()).thenReturn("这是一道比较简单的题目");
+        when(homeworkQuiz.getEvaluateScript()).thenReturn("www.baidu.com");
+        when(homeworkQuiz.getTemplateRepository()).thenReturn("templateRepository");
+
+        when(homeworkQuizMapper.findById(2)).thenReturn(homeworkQuiz);
+        when(homeworkQuiz.getDescription()).thenReturn("这是一道普通难度的题目");
+        when(homeworkQuiz.getEvaluateScript()).thenReturn("www.baidu.com");
+        when(homeworkQuiz.getTemplateRepository()).thenReturn("templateRepository");
+
+        Response response = target(basePath + "/ids/1,2").request().get();
+        assertThat(response.getStatus(), is(200));
+    }
 }
