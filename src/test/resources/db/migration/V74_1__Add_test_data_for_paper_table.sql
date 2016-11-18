7f0cf9e78efd23ce00919488a7f59edc4fb19b63
Miss-qi <1370322806@qq.com>
2016-11-18 14:17:39 +0800

    change sql test

--- src/test/resources/db/migration/V74_1__Add_test_data_for_paper_table.sql
+++ src/test/resources/db/migration/V74_1__Add_test_data_for_paper_table.sql
@@ -1,8 +1,8 @@
 --TRUNCATE paper;
-INSERT INTO paper values(null, 3,"java","a","2017",false);
-INSERT INTO paper values(null, 2,"js","b","2014",false);
-INSERT INTO paper values(null, 2,"c++","c","2013",false);
-INSERT INTO paper values(null, 5,"c","d","2016",false);
-INSERT INTO paper values(null, 2,"ruby","e","2015",true);
-INSERT INTO paper values(null, 3,"scale","e","2015",true);
-INSERT INTO paper values(null, 7,"react","e","2015",true);
+INSERT INTO paper values(null, 3,"java","a","2017",false, 6);
+INSERT INTO paper values(null, 2,"js","b","2014",false, 6);
+INSERT INTO paper values(null, 2,"c++","c","2013",false, 6);
+INSERT INTO paper values(null, 5,"c","d","2016",false, 6);
+INSERT INTO paper values(null, 2,"ruby","e","2015",true, 6);
+INSERT INTO paper values(null, 3,"scale","e","2015",true, 6);
+INSERT INTO paper values(null, 7,"react","e","2015",true, 6);
