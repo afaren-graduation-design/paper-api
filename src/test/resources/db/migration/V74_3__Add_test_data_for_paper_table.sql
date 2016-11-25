450908d3f39b3c12832587fd6050a8bed70ec8d2
Miss-qi <1370322806@qq.com>
2016-11-25 21:00:15 +0800

    update data for paper table

---
+++ src/test/resources/db/migration/V74_3__Add_test_data_for_paper_table.sql
@@ -0,0 +1,7 @@
+UPDATE paper SET programId=1 WHERE id=1;
+UPDATE paper SET programId=3 WHERE id=2;
+UPDATE paper SET programId=2 WHERE id=3;
+UPDATE paper SET programId=2 WHERE id=3;
+UPDATE paper SET makerId=2,paperName="测试试卷",description="f",createTime="2016",isDistribution=0,programId=6 where id=4;
+
+
