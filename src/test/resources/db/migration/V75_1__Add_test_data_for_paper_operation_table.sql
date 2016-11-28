fcfcbbf48a1f91c720169a2cba6b208a0473d17c
YingjiaZhang <zhyingjia@163.com>
2016-11-28 16:25:29 +0800

    add a table about paperOperation and init data

---
+++ src/test/resources/db/migration/V75_1__Add_test_data_for_paper_operation_table.sql
@@ -0,0 +1,10 @@
+INSERT INTO paperOperation VALUES (NULL ,"delete",1,1453287441,1);
+INSERT INTO paperOperation VALUES (NULL ,"update",1,1453287441,2);
+INSERT INTO paperOperation VALUES (NULL ,"distribution",1,1453287441,3);
+INSERT INTO paperOperation VALUES (NULL ,"add",2,1453287441,4);
+INSERT INTO paperOperation VALUES (NULL ,"restore",2,1453287441,5);
+
+
+
+
+
