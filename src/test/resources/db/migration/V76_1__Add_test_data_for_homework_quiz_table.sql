d5bd3542c4f555a53c8e51a4cc31d772c6ebce03
YingjiaZhang <zhyingjia@163.com>
2016-11-29 15:34:46 +0800

    zyj & qyz - update homeworkQuiz table structure and update data about it

---
+++ src/test/resources/db/migration/V76_1__Add_test_data_for_homework_quiz_table.sql
@@ -0,0 +1,8 @@
+UPDATE homeworkQuiz SET makerId=1,createTime=123456,homeworkName="homework1"   WHERE id = 1;
+UPDATE homeworkQuiz SET makerId=2,createTime=123457,homeworkName="homework2"   WHERE id = 2;
+UPDATE homeworkQuiz SET makerId=2,createTime=123458,homeworkName="homework3"   WHERE id = 3;
+UPDATE homeworkQuiz SET makerId=1,createTime=123459,homeworkName="homework4"   WHERE id = 4;
+UPDATE homeworkQuiz SET makerId=1,createTime=123450,homeworkName="homework5"   WHERE id = 5;
+UPDATE homeworkQuiz SET makerId=1,createTime=123450,homeworkName="homework6"   WHERE id = 6;
+UPDATE homeworkQuiz SET makerId=1,createTime=123450,homeworkName="homework7"   WHERE id = 7;
+UPDATE homeworkQuiz SET makerId=1,createTime=123450,homeworkName="homework8"   WHERE id = 8;
