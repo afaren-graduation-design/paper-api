59d224a63f26b836bed9ac3f1a2dbf019ea3bc6e
Miss-qi <1370322806@qq.com>
2016-11-29 17:10:10 +0800

    zyj & qyz - add interface about post /program/{programId}/paper/{paperId}

--- src/main/java/com/thoughtworks/twars/resource/ProgramResource.java
+++ src/main/java/com/thoughtworks/twars/resource/ProgramResource.java
@@ -1,13 +1,19 @@
 package com.thoughtworks.twars.resource;
 
-import com.thoughtworks.twars.bean.*;
-import com.thoughtworks.twars.mapper.*;
-import io.swagger.annotations.*;
+import com.thoughtworks.twars.bean.Paper;
+import com.thoughtworks.twars.bean.PaperOperation;
+import com.thoughtworks.twars.mapper.PaperMapper;
+import com.thoughtworks.twars.mapper.PaperOperationMapper;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 
 import javax.inject.Inject;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.util.Map;
 
 @Path("/program")
 @Api
@@ -15,6 +21,8 @@ public class ProgramResource extends Resource {
 
     @Inject
     private PaperMapper paperMapper;
+    @Inject
+    private PaperOperationMapper paperOperationMapper;
 
     @GET
     @ApiResponses(value = {@ApiResponse(code = 200, message = "get one paper from program successfully"),
@@ -34,5 +42,36 @@ public class ProgramResource extends Resource {
 
         return Response.status(Response.Status.OK)
                 .entity(paper.getResponseInfo()).build();
+    }
+
+    @POST
+    @ApiResponses(value = {@ApiResponse(code = 204, message = "delete paper successfully"),
+            @ApiResponse(code = 404, message = "delete paper failed")})
+    @Path("/{programId}/paper/{paperId}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response insertPaperOperation(
+            @ApiParam(name = "programId", value = "programId", required = true)
+            @PathParam("programId") int programId,
+            @ApiParam(name = "paperId", value = "paperId", required = true)
+            @PathParam("paperId") int paperId,
+            @ApiParam(name = "data", value = "include all info about paper operation when delete paper", required = true)
+                    Map data) {
+
+        String operationType = (String) data.get("operationType");
+        int operatorId = (int) data.get("operatorId");
+        int operatingTime = (int) data.get("operatingTime");
+
+        PaperOperation paperOperation = new PaperOperation();
+        paperOperation.setOperationType(operationType);
+        paperOperation.setOperatorId(operatorId);
+        paperOperation.setOperatingTime(operatingTime);
+        paperOperation.setPaperId(paperId);
+
+        int result = paperOperationMapper.insertPaperOperation(paperOperation);
+        if (result == 0){
+            return Response.status(Response.Status.NOT_FOUND).build();
+        }
+        return Response.status(Response.Status.NO_CONTENT).build();
+
     }
 }
