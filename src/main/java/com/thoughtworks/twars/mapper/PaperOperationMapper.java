428c376f57b3836764a2be31d0f52c3c62868693
Miss-qi <1370322806@qq.com>
2016-11-29 16:59:26 +0800

    zyj & qyz - add a delete interface about /program/1/paper/1

---
+++ src/main/java/com/thoughtworks/twars/mapper/PaperOperationMapper.java
@@ -0,0 +1,7 @@
+package com.thoughtworks.twars.mapper;
+
+import com.thoughtworks.twars.bean.PaperOperation;
+
+public  interface PaperOperationMapper {
+    int insertPaperOperation(PaperOperation paperOperation);
+}
