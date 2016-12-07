ba7062906f79ea01ea293bf6c9d2f5f9b13a17d0
Miss-qi <1370322806@qq.com>
2016-12-07 12:11:55 +0800

    delete unnecessary sentence at ProgramResourceTest.java

--- src/test/java/com/thoughtworks/twars/resource/ProgramResourceTest.java
+++ src/test/java/com/thoughtworks/twars/resource/ProgramResourceTest.java
@@ -1,9 +1,6 @@
 package com.thoughtworks.twars.resource;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 import com.thoughtworks.twars.bean.*;
-import com.thoughtworks.twars.mapper.PaperMapper;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
@@ -13,17 +10,9 @@ import javax.ws.rs.core.Response;
 import java.util.*;
 
 import static org.mockito.Mockito.when;
-import javax.ws.rs.client.Entity;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
-
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
-import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
 public class ProgramResourceTest extends TestBase {
@@ -31,9 +20,9 @@ public class ProgramResourceTest extends TestBase {
 
     @Mock
     Paper paper;
-    
+
     @Test
-    public void should_list_all_papers_by_page_and_pageSize() throws Exception {
+    public void should_list_all_papers() throws Exception {
 
         when(paperMapper.getOnePaper(2)).thenReturn(paper);
 
@@ -52,13 +41,12 @@ public class ProgramResourceTest extends TestBase {
         section2.setId(2);
         section2.setDescription("just a test 2");
         section2.setType("homeworkQuizzes");
+
         sections.add(section2);
 
         when(paper.getSections()).thenReturn(sections);
 
         Response response = target(basePath + "/1/paper/2").request().get();
-
-        Paper onePaper = response.readEntity(Paper.class);
 
         assertThat(response.getStatus(), is(200));
 
