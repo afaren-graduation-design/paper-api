89d019ffc11e8dbeaa3e7d2fb8ff56f91042ba9f
YingjiaZhang <zhyingjia@163.com>
2016-12-07 20:47:23 +0800

    add test about /program/{programId}/papers

--- src/test/java/com/thoughtworks/twars/resource/ProgramResourceTest.java
+++ src/test/java/com/thoughtworks/twars/resource/ProgramResourceTest.java
@@ -1,6 +1,9 @@
 package com.thoughtworks.twars.resource;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.thoughtworks.twars.bean.*;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
@@ -20,6 +23,16 @@ public class ProgramResourceTest extends TestBase {
 
     @Mock
     Paper paper;
+
+    @Mock
+    List<Paper> papers;
+
+    @Mock
+    Paper firstPaper;
+
+    @Mock
+    Paper secondPaper;
+
 
     @Test
     public void should_list_all_papers() throws Exception {
@@ -50,5 +63,36 @@ public class ProgramResourceTest extends TestBase {
 
         assertThat(response.getStatus(), is(200));
 
+    }
+
+    @Test
+    public void should_list_all_papers_by_programId() throws Exception {
+
+        when(paperMapper.findPapersByProgramId(6)).thenReturn(Arrays.asList(firstPaper, secondPaper));
+        when(papers.size()).thenReturn(2);
+
+        when(firstPaper.getId()).thenReturn(1);
+        when(firstPaper.getPaperName()).thenReturn("简单的试卷");
+        when(firstPaper.getDescription()).thenReturn("easy");
+        when(firstPaper.getCreateTime()).thenReturn("2016-11-11");
+        when(firstPaper.getMakerId()).thenReturn(3);
+        when(firstPaper.getIsDistribution()).thenReturn(true);
+
+        when(secondPaper.getId()).thenReturn(5);
+        when(secondPaper.getPaperName()).thenReturn("普通的试卷");
+        when(secondPaper.getDescription()).thenReturn("common");
+        when(secondPaper.getCreateTime()).thenReturn("2016-11-12");
+        when(secondPaper.getMakerId()).thenReturn(2);
+        when(secondPaper.getIsDistribution()).thenReturn(false);
+
+        Response response = target(basePath+"/6/papers").request().get();
+        Assert.assertThat(response.getStatus(), is(200));
+
+        Gson gson = new GsonBuilder().create();
+
+        Map result = response.readEntity(Map.class);
+        String jsonStr = gson.toJson(result);
+
+        Assert.assertThat(jsonStr, is("{\"paperList\":[{\"createTime\":\"2016-11-11\",\"paperName\":\"简单的试卷\",\"description\":\"easy\",\"isDistribution\":true,\"id\":1,\"uri\":\"/papers/1\",\"programId\":0,\"makerId\":3},{\"createTime\":\"2016-11-12\",\"paperName\":\"普通的试卷\",\"description\":\"common\",\"isDistribution\":false,\"id\":5,\"uri\":\"/papers/5\",\"programId\":0,\"makerId\":2}]}"));
     }
 }
