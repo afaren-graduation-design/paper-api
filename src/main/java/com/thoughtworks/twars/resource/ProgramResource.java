c7c1a18820aa626bca7ba4c73c7e0064f87e1127
YingjiaZhang <zhyingjia@163.com>
2016-12-08 18:59:24 +0800

    restore last

--- src/main/java/com/thoughtworks/twars/resource/ProgramResource.java
+++ src/main/java/com/thoughtworks/twars/resource/ProgramResource.java
@@ -83,7 +83,7 @@ public class ProgramResource extends Resource {
         }
 
         return Response.status(Response.Status.OK)
-                .entity(paper).build();
+                .entity(paper.getResponseInfo()).build();
     }
 
     @POST
