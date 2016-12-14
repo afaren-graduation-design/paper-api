c64cf91ff8134c3bbda48276c0cba59e2cbcb11f
rangel <18792561236@163.com>
2016-12-14 18:03:14 +0800

    Add test

--- src/main/java/com/thoughtworks/twars/resource/ProgramResource.java
+++ src/main/java/com/thoughtworks/twars/resource/ProgramResource.java
@@ -76,7 +76,7 @@ public class ProgramResource extends Resource {
     @Path("/{programId}/papers")
     @Produces(MediaType.APPLICATION_JSON)
     public Response insertPapers(
-            @ApiParam(name = "data", value = "all insert paper", required = true) Map data,
+            @ApiParam(name = "data", value = "insert paper", required = true) Map data,
             @PathParam("programId") int programId) {
 
         Integer makerId = (Integer) data.get("makerId");
@@ -151,8 +151,12 @@ public class ProgramResource extends Resource {
                     sectionQuizMapper.insertSectionQuiz(sectionQuiz);
 
                 }
+
             }
+
+
         }
+
 
         Map result = new HashMap();
         result.put("uri", "programs/" + programId + "/papers/" + paperId);
--- src/test/java/com/thoughtworks/twars/resource/ProgramResourceTest.java
+++ src/test/java/com/thoughtworks/twars/resource/ProgramResourceTest.java
@@ -9,6 +9,8 @@ import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.util.*;
 
@@ -101,4 +103,57 @@ public class ProgramResourceTest extends TestBase {
                 + "\"description\":\"common\",\"isDistribution\":false,\"id\":5,"
                 + "\"uri\":\"/papers/5\",\"programId\":0,\"makerId\":2}]}"));
     }
+
+    @Test
+    public void should_create_papers() throws Exception {
+
+        Map data = new HashMap();
+        data.put("makerId", 1);
+        data.put("createTime", 111111);
+        data.put("programId", 1);
+        data.put("paperName", "paperName");
+        data.put("description", "paper desc");
+
+        List sections = new ArrayList();
+        data.put("sections",sections);
+
+        Map sectionOne = new HashMap();
+        Map sectionTwo = new HashMap();
+        sections.add(sectionOne);
+        sections.add(sectionTwo);
+
+        sectionOne.put("type","blankQuizzes");
+        sectionOne.put("description","blankQuizzes description");
+
+        Map itemsOne = new HashMap();
+        sectionOne.put("items",itemsOne);
+        itemsOne.put("easyCount", 1);
+        itemsOne.put("normalCount", 1);
+        itemsOne.put("hardCount", 1);
+        itemsOne.put("exampleCount", 1);
+
+        sectionTwo.put("type","homeworkQuizzes");
+        sectionTwo.put("description","blankQuizzes description");
+
+        List itemsTwo = new ArrayList();
+        sectionTwo.put("items",itemsTwo);
+
+        Map homeworkOne = new HashMap();
+        homeworkOne.put("id",1);
+        homeworkOne.put("uri","homeworkQuizzes/1");
+
+        Map homeworkTwo = new HashMap();
+        homeworkTwo.put("id",2);
+        homeworkTwo.put("uri","homeworkQuizzes/2");
+
+        itemsTwo.add(homeworkOne);
+        itemsTwo.add(homeworkTwo);
+
+        Entity entity = Entity.entity(data, MediaType.APPLICATION_JSON_TYPE);
+        Response response = target(basePath + "/1/papers").request().post(entity);
+
+        assertThat(response.getStatus(), is(201));
+
+    }
+
 }
