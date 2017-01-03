7fdafab358bb23891ccc3acce960ccbc5c4e944d
rangel <18792561236@163.com>
2017-01-03 17:06:17 +0800

    Modify some codes

---
+++ src/main/java/com/thoughtworks/twars/bean/stack.java
@@ -0,0 +1,7 @@
+package com.thoughtworks.twars.bean;
+
+/**
+ * Created by guoru on 16-12-30.
+ */
+public class stack {
+}
--- src/main/java/com/thoughtworks/twars/resource/RelationshipCreatingResource.java
+++ src/main/java/com/thoughtworks/twars/resource/RelationshipCreatingResource.java
@@ -23,7 +23,7 @@ public class RelationshipCreatingResource extends Resource {
     @ApiResponses(value = {@ApiResponse(code = 201,
             message = "create mentor by userId successfully"),
             @ApiResponse(code = 403, message = "create mentor by userId failure")})
-    @Path("/{mentorId}/users/{studentId}")
+    @Path("/{mentorId}/students/{studentId}")
     @Produces(MediaType.APPLICATION_JSON)
     public Response insertMentorUser(
             @PathParam("mentorId") int mentorId,
