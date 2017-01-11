9f3b55b478bdd6f36345471adea759049f67fc9a
Afaren <550047450@qq.com>
2017-01-11 15:07:26 +0800

    afaren - add a database config

--- src/test/java/com/thoughtworks/twars/mapper/TestBase.java
+++ src/test/java/com/thoughtworks/twars/mapper/TestBase.java
@@ -1,6 +1,7 @@
 package com.thoughtworks.twars.mapper;
 
 import com.thoughtworks.twars.util.DatabaseUtil;
+import com.thoughtworks.twars.util.EnvUtil;
 import org.apache.ibatis.session.SqlSessionManager;
 import org.flywaydb.core.Flyway;
 import org.junit.After;
@@ -9,10 +10,14 @@ import org.junit.Before;
 import javax.sql.DataSource;
 
 public class TestBase {
-    protected SqlSessionManager session = DatabaseUtil.getSession("test");
+
+    protected  SqlSessionManager session;
+
     Flyway flyway = new Flyway();
 
     {
+        String env =  EnvUtil.getVariable("ci_test") != null ? "ci_test" : "test";
+        session = DatabaseUtil.getSession(env);
         DataSource ds = session.getConfiguration().getEnvironment().getDataSource();
         flyway.setDataSource(ds);
     }
