be6222884b75e19e2e07f5018ba40f8de689501e
Yvette14 <libra1014tyb@163.com>
2017-02-17 16:03:55 +0800

    change stack table word

--- src/main/resources/db/migration/V83__Create_stack_table.sql
+++ src/main/resources/db/migration/V83__Create_stack_table.sql
@@ -2,5 +2,5 @@ create table stack(
    stackId INT not null primary key AUTO_INCREMENT,
    title VARCHAR(128) NOT NULL  UNIQUE,
    description VARCHAR(128) NOT NULL,
-   definitionFile VARCHAR(128) NOT NULL
+   definition VARCHAR(128) NOT NULL
 ) DEFAULT CHARSET=utf8;
