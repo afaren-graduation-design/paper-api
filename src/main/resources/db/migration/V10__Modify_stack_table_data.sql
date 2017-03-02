9c80ce204c80a3ba3c1509148d75d5aa2108f6b8
YingjiaZhang <zhyingjia@163.com>
2017-03-02 16:33:09 +0800

    add rename sql file

--- src/main/resources/db/migration/V10__Modify_stack_table_data.sql
+++
@@ -1,5 +0,0 @@
-update stack set title = "Php" where stackId = 1;
-update stack set title = "Python" where stackId = 2;
-update stack set definition = "Php:5" where stackId = 1;
-update stack set definition = "Python:2" where stackId = 2;
-update stack set definition = "Ruby:1" where stackId = 3;
