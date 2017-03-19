3dd2df6212953045e2e5f32340b03edafa3bf5cb
Afaren <550047450@qq.com>
2017-03-19 22:13:05 +0800

    afaren - fixbug: remove userName cause there is already name in userDetail table

--- src/main/java/com/thoughtworks/twars/bean/User.java
+++ src/main/java/com/thoughtworks/twars/bean/User.java
@@ -65,7 +65,16 @@ public class User {
         result.put("id",getId());
         result.put("email", getEmail());
         result.put("mobilePhone", getMobilePhone());
-        result.put("userName", getUserName());
+
+        // FIXME: 3/19/17
+//        数据库 users 表存在 userName 字段，用户注册之后为 NULL
+//        userDetail 表存在 name 字段，用户注册后正常写入
+//        /user/{id}/detail 接口同时返回 userName 跟 name，
+//        web-api 接收这个接口的响应时判断 userDetail 是否有字段为空，
+//        如果有空字段，立即返回 false，前端页面跳转到 userCenter，让用户补充填写
+//        这里的bug 为数据不一致，或者说，同一份数据存在了两个地方，导致不一致
+
+//        result.put("userName", getUserName());
         result.put("role", roles);
 
         return result;
